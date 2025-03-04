1a98cd4e132cdd61688b776bc8d31c61364a5f8d
Mauricio <mauriachetta98@gmail.com>
2025-03-04 18:02:06 -0300

    Pruebas del Hook useSate

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
   const [count, setCount] = useState(0)
+
+  const [usuario, setUsuario] = useState("")
+
+  
+
+  function incrementCounter(){
+    setCount(count + 1)
+  }
+
+  function updateUserName(e){
+const name = e.target.value;
+
+    setUsuario(name)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Contador</h1>
+      <h2>Valor actual: {count}</h2>
+      <button onClick={()=> incrementCounter() }>Add +</button>
+
+      <hr />
+
+      <input type="text" onKeyUp={(evt) => updateUserName(evt)} />
+      <h3>Bienvenido {usuario}</h3>
     </>
   )
 }
