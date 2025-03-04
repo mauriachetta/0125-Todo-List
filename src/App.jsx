b7a5acdb81bd0c2e5adf67195a148b3743956bdb
Mauricio <mauriachetta98@gmail.com>
2025-03-04 18:43:55 -0300

    Cambios trabajando con TODO-LIST

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,55 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+const TODO_LIST = [
+  {id:1, text:"Tarea 1", completed:false},
+  {id:1, text:"Tarea 2", completed:false},
+  {id:1, text:"Tarea 3", completed:false},
+  {id:1, text:"Tarea 4", completed:false},
+  {id:1, text:"Tarea 5", completed:false}
+
+]
+
 function App() {
 
-  const [count, setCount] = useState(0)
-
-  const [usuario, setUsuario] = useState("")
-
-  
-
-  function incrementCounter(){
-    setCount(count + 1)
-  }
-
-  function updateUserName(e){
-const name = e.target.value;
-
-    setUsuario(name)
-  }
 
 
   return (
     <>
-      <h1>Contador</h1>
-      <h2>Valor actual: {count}</h2>
-      <button onClick={()=> incrementCounter() }>Add +</button>
+      <h1 className='todo-list-title'>
+        Todo List React:
+      </h1>
 
       <hr />
 
-      <input type="text" onKeyUp={(evt) => updateUserName(evt)} />
-      <h3>Bienvenido {usuario}</h3>
+      <div className="input-container">
+        <label htmlFor="">Agregar tarea:</label>
+        <input type="text" />
+      </div>
+
+      <div className='todo-list-container'>
+
+        <ul className="todo-list">
+
+          {
+            TODO_LIST.map((todo)=>{
+              return(
+                <li className="todo-item">
+                <div className="todo-left">
+                  <input type="checkbox" className='todo-checkbox' />
+                  <span className='todo-item'>
+                    {todo.text}
+                  </span>
+    
+                </div>
+                <button className="todo-dele">Eliminar</button>
+              </li>
+              )
+            })
+          }
+
+        </ul>
+      </div>
     </>
   )
 }
